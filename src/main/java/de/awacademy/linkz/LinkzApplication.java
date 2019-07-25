7b8a715c25ac7f4529dcaa83f559c1dcad8c8610
Philip Frank <mail@philipfrank.de>
2019-07-25 14:18:30 +0200

    initial version

---
+++ src/main/java/de/awacademy/linkz/LinkzApplication.java
@@ -0,0 +1,13 @@
+package de.awacademy.linkz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/awacademy/linkz/LinkzApplicationTests.java
@@ -0,0 +1,16 @@
+package de.awacademy.linkz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LinkzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
